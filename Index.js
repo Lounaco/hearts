4ee6a3669ff2bfba400ae8be91498f377d8b8216
Lounaco <squirrelonair@gmail.com>
2023-02-04 10:52:54 +0300

    Last_commit_in_the_project

--- Index.js
+++ Index.js
@@ -6,7 +6,14 @@
     const divEl = document.createElement("div");
     divEl.style.left = xPos + "px";
     divEl.style.top = yPos + "px";
+    const size = Math.random()*100;
+    divEl.style.width = size + "px";
+    divEl.style.height = size + "px";
     bodyEl.appendChild(divEl);
+    setTimeout(()=>{
+      divEl.remove();
+
+    }, 5000)
 
  }
  )
